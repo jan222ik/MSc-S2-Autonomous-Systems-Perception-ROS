aab9db8d9353e318299a8052be3f3ece3c2d39a5
Janik <jan.222.ik@gmail.com>
2021-05-06 13:55:54 -0700

    Fixed tcp communication and added documentation

---
+++ src/collab/README.md
@@ -0,0 +1,58 @@
+# MSc-S2-Autonomous-Systems-Perception-ROS - collab
+
+Package with launch files to start multiple roboter at once and establish communication between them via TCP.
+
+## Clone:
+This shell script copies the files of this package to your PC and removes the repository later.
+Be in Working Dir: ``~/catkin_ws/src``
+```shell
+mkdir collab
+mkdir MSc-S2-Autonomous-Systems-Perception-ROS
+git clone https://github.com/jan222ik/MSc-S2-Autonomous-Systems-Perception-ROS --no-checkout MSc-S2-Autonomous-Systems-Perception-ROS --depth 1
+cd MSc-S2-Autonomous-Systems-Perception-ROS
+git sparse-checkout init --cone
+git sparse-checkout set src/collab
+cp -R src/collab/* ../collab
+cd ..
+rm -rf MSc-S2-Autonomous-Systems-Perception-ROS
+```
+
+## Launch Files
+
+### 1. Environment - emptyWorld.launch
+Loads an empty world inside Gazebo.
+
+### 2. Roboter - single.launch
+Launch file for a single roboter. 
+It spawns the roboter and creates a TCP-ClientServer node for communication.
+It takes arguments for the roboter:
+  - robot_name: Name of the robot
+  - init_pose: Initial Pose in shape ``"-x -1 -y 1 -z 0"``
+  - model: Name of turtlebot
+  - tcpFlag: Determines if the robot is a tcp server or a tcp client.
+The launch file also expects the param robot_description to be set.
+
+### 3. Two Roboter combined - multi.launch
+Starts two roboter in different namespaces, defines args and params required by single.launch and stats
+a terminal with a teleop node for each of the turtles.
+
+**⚠ WARNING - Teleop Terminal**: The used terminal is started with ``gnome-terminal -e``; thus the executing
+system must have this terminal available.
+
+```shell
+roslaunch collab multi.launch
+
+```
+
+## TCP Node - tcpCommunication.py
+
+Handles TCP communication the node can instantiate either a server, or a client based on the provided
+flag. 
+Due to the asynchronous nature of the environment a connection on the first try is unlikely or at least not expected.
+Hence, the client will retry the connection after a short delay. The client will retry up to 10 times before
+forfeiting.
+
+The robots will print each other linear x and angular z velocities in the console after they are successfully connected.
+
+
+
--- src/collab/src/tcpCommunication.py
+++ src/collab/src/tcpCommunication.py
@@ -3,17 +3,20 @@
 import rospy
 import sys
 import socket
-from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
 import time
 
 
 class ClientServer:
-    def __init__(self, port, isServer, turtle):
+    def __init__(self, isServer, turtle):
         self.isServer = isServer
-        self.sub = rospy.Subscriber(turtle + "/pose", Pose, self.nextPose)
+        poseTopic = rospy.get_namespace() + "cmd_vel"
+        self.sub = rospy.Subscriber(poseTopic, Twist, self.nextTwist)
+        rospy.logdebug("Subscribe for Pose on topic: %s" % poseTopic)
         self.sock = None
         self.latestPose = None
-        self.connect(port)
+        self.hasConnection = False
+        self.awaitRes = False
 
     def connect(self, port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,29 +24,54 @@ class ClientServer:
             self.sock.bind(("127.0.0.1", port))
             self.server()
         else:
-            self.sock.connect(("127.0.0.1", port))
+            tries = 10
+            while tries > 0 and not self.hasConnection:
+                tries -= 1
+                try:
+                    self.sock.connect(("127.0.0.1", port))
+                    rospy.logdebug("Client connected")
+                    self.hasConnection = True
+                except (OSError, ConnectionRefusedError) as e:
+                    rospy.logdebug("Client connection failed: %d tries remaining." % tries)
+                    # Close old socket and create new socket
+                    self.sock.close()
+                    time.sleep(10)
+                    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def nextPose(self, msg):
-        rospy.loginfo("Next Pose")
+    def nextTwist(self, msg):
         self.latestPose = msg
-        if not self.isServer:
-            self.sock.send(msg.encode())
-            res: str = self.sock.recv(1024)
-            if res:
-                rospy.loginfo("From Server - Incoming: ", res)
+
+    def send(self):
+        if self.latestPose is None:
+            return
+        try:
+            self.awaitRes = True
+            self.sock.sendall(bytes("x: " + str(self.latestPose.linear.x) + " ang.z: " + str(self.latestPose.angular.z), "utf-8"))
+            while self.awaitRes:
+                res: str = str(self.sock.recv(1024), "utf-8")
+                if res:
+                    rospy.loginfo("From Server - Incoming: %s" % res)
+                    self.awaitRes = False
+        except BrokenPipeError as e:
+            rospy.logerr("Broken Pipe at Client 2")
+            self.awaitRes = False
+            pass
+        time.sleep(1)
 
     def server(self):
         self.sock.listen(1)
-        while True:
-            con, _ = self.sock.accept()
+        con, _ = self.sock.accept()
+        self.hasConnection = True
+        while self.hasConnection:
             try:
-                res: str = con.recv(1024).decode()
+                res: str = str(con.recv(1024), "utf-8")
                 if res:
-                    rospy.loginfo("From Client - Incoming: ", res)
-                    con.send(self.latestPose.encode())
-            finally:
-                con.close()
-                return
+                    rospy.loginfo("From Client - Incoming: %s" % str(res))
+                    con.sendall(bytes("x: " + str(self.latestPose.linear.x) + " ang.z: " + str(self.latestPose.angular.z), "utf-8"))
+            except BrokenPipeError as e:
+                print("Broken Pipe at Server")
+                pass
+        con.close()
 
     def close(self):
         self.sock.close()
@@ -55,32 +83,27 @@ if __name__ == '__main__':
         rospy.logerr("Insufficient arguments - please provide: -s if server or -c for client and turtle name")
     else:
         rospy.loginfo("Args" + str(sys.argv))
-        tries = 10
         it = None
         try:
-            while tries > 0:
-                tries -= 1
-                try:
-                    if sys.argv[1] == '-s':
-                        rospy.init_node("tcp_server", log_level=rospy.DEBUG, anonymous=True)
-                        it = ClientServer(
-                            port=port,
-                            isServer=True,
-                            turtle=sys.argv[2]
-                        )
-                    else:
-                        rospy.init_node("tcp_client", log_level=rospy.DEBUG, anonymous=True)
-                        it = ClientServer(
-                            port=port,
-                            isServer=False,
-                            turtle=sys.argv[2]
-                        )
-                    rospy.spin()
-                except ConnectionRefusedError:
-                    pass
-                finally:
-                    it.close()
-                time.sleep(10000)
+            isServer = sys.argv[1] == '-s'
+            if isServer:
+                rospy.init_node("tcp_server", log_level=rospy.DEBUG, anonymous=True)
+                it = ClientServer(
+                    isServer=isServer,
+                    turtle=sys.argv[2]
+                )
+            else:
+                rospy.init_node("tcp_client", log_level=rospy.DEBUG, anonymous=True)
+                it = ClientServer(
+                    isServer=isServer,
+                    turtle=sys.argv[2]
+                )
+            it.connect(port)
+            if isServer:
+                rospy.spin()
+            else:
+                while not rospy.is_shutdown():
+                    it.send()
         except rospy.ROSInterruptException:
             it.close()
             pass
